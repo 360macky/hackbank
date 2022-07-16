8ed366483a4e072d16d433941ec8e3835934cd64
Marcelo Arias <mail.marcelo.as@gmail.com>
2022-07-16 18:50:57 -0500

    Create Explore component

---
+++ assets/Explore.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Svg, { Path } from 'react-native-svg';
+
+export default function Explore() {
+  return (
+    <Svg
+      width="48"
+      height="48"
+      viewBox="0 0 48 48"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <Path
+        d="M24 21.8C22.78 21.8 21.8 22.78 21.8 24C21.8 25.22 22.78 26.2 24 26.2C25.22 26.2 26.2 25.22 26.2 24C26.2 22.78 25.22 21.8 24 21.8ZM24 4C12.96 4 4 12.96 4 24C4 35.04 12.96 44 24 44C35.04 44 44 35.04 44 24C44 12.96 35.04 4 24 4ZM28.38 28.38L12 36L19.62 19.62L36 12L28.38 28.38Z"
+        fill="#006C4B"
+      />
+    </Svg>
+  );
+}
