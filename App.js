8f3237fdc43b9a13f66225b8679a09fb9c027f9f
Marcelo Arias <mail.marcelo.as@gmail.com>
2022-07-15 22:47:36 -0500

    Update App.js to add more screens

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import Form from './src/screens/Form';
+import Prediction from './src/screens/Prediction';
+import About from './src/screens/About';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Hackbank"
+          screenOptions={{ headerShown: false }}
+        >
+          <Stack.Screen name="Hackbank" component={Form} />
+          <Stack.Screen name="Prediction" component={Prediction} />
+          <Stack.Screen name="Acerca de" component={About} />
+        </Stack.Navigator>
+      </NavigationContainer>
+      <StatusBar
+        backgroundColor="#006C4B"
+        style="light"
+        barStyle="dark-content"
+      />
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
