8c6f23d5adad9b92f728af3fb9215680f77533ab
Marcelo Arias <mail.marcelo.as@gmail.com>
2022-07-16 18:45:54 -0500

    Create FormLogo component

---
+++ assets/FormLogo.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Svg, { Path, Rect } from 'react-native-svg';
+
+export default function FormLogo() {
+  return (
+    <Svg
+      width="44"
+      height="66"
+      viewBox="0 0 44 66"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <Path
+        d="M3.42441 49.3L0 44.2952H7.11094V18.7825H3.42441L0 13.7532H19.3668V18.7825H15.6803V28.5481H28.6197V18.7825H24.9332V13.7532H44L40.8512 18.7825H37.1646V44.2952H44L40.8512 49.3H24.9332V44.2952H28.6197V35.1155H15.6803V44.2952H19.3668V49.3H3.42441Z"
+        fill="#CFE8D9"
+      />
+      <Path
+        d="M21.6407 50.2999L36.2136 50.2999L37.7073 50.2999L43.5 50.2999L21.6407 65.5999L1.33757e-06 50.2999L7.28643 50.2999L21.6407 50.2999Z"
+        fill="#CFE8D9"
+      />
+      <Rect x="0.300049" y="9.1001" width="43.6" height="3.7" fill="#CFE8D9" />
+      <Rect x="0.400146" width="43.5" height="8.1" fill="#CFE8D9" />
+    </Svg>
+  );
+}
